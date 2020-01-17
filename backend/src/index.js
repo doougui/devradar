db47b6b51e1749d7df8da97f2101a70962ccef00
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2020-01-17 12:49:16 -0300

    Aula 5 - Funcionalidades avançadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 require('dotenv/config');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(`mongodb+srv://doougui:${process.env.DATABASE_PASS}@cluster0-i7hyt.mongodb.net/week10?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
@@ -17,4 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
