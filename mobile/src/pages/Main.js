696c46ba257239a9b067bd16632a5bc9b3581ac2
Douglas Pinheiro Goulart <douglaspigoulart@gmail.com>
2020-01-18 14:54:50 -0300

    Adicionando estilos em um arquivo separado

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
 import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
+
+import styles from './styles';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -146,72 +148,5 @@ function Main({ navigation }) {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  map: {
-    flex: 1,
-  },
-
-  avatar: {
-    width: 54,
-    height: 54,
-    borderRadius: 4,
-    borderWidth: 4,
-    borderColor: '#FFF',
-  },
-
-  callout: {
-    width: 260,
-  }, 
-
-  devName: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-
-  devBio: {
-    color: '#666',
-    marginTop: 5,
-  },
-
-  devTechs: {
-    marginTop: 5,
-  },
-
-  searchForm: {
-    position: 'absolute',
-    left: 20,
-    right: 20,
-    zIndex: 5,
-    flexDirection: 'row',
-  },
-
-  searchInput: {
-    flex: 1,
-    height: 50,
-    backgroundColor: '#FFF',
-    color: '#333',
-    borderRadius: 25,
-    paddingHorizontal: 20,
-    fontSize: 16,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowOffset: {
-      width: 4,
-      height: 4,
-    },
-    elevation: 2,
-  },
-
-  loadButton: {
-    width: 50,
-    height: 50,
-    backgroundColor: '#8E4DFF',
-    borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginLeft: 15,
-  },
-});
 
 export default Main;
